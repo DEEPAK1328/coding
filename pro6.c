0ca3bed3e768b6940d29fb633f16d1ce59ae422e
DEEPAK <k.appu25@gcu.edu.in>
2025-11-09 21:07:50 +0530

    Delete pro6.c
    .

--- pro6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float p , r , t , si;
-    printf("entre principal, rate, time: ");
-    scanf("%f %f %f" , &p , &r , &t);
-    si = (p * r * t) / 100;
-    printf ("simple intrest = %2f" , si);
-    return 0;
-}
